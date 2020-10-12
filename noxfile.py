e5007e86b6ecef89cf30811a6b878c29a56ee809
Sebastian <Seburath@gmail.com>
2020-10-11 22:31:37 -0500

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -5,16 +5,9 @@ import nox
 def tests(session):
     session.install('-r', 'requirements.txt')
     session.run('nikola', 'build', '--strict')
-    session.run('nikola', 'check', '--check-links', '--check-files')
 
 
 @nox.session
 def lint_rst(session):
     session.install('rstcheck')
     session.run('rstcheck', '-r', '.')
-
-
-@nox.session
-def lint_yaml(session):
-    session.install('yamllint')
-    session.run('yamllint', '-s', 'data')
